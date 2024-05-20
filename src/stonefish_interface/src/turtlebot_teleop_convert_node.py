19c885c50df76da26627473e6349708c07abfc2e
LeoPhamVN <phamthanhloc.bkhn@gmail.com>
2024-05-20 14:54:10 +0200

    added teleop convert node to stonefish interface pack

---
+++ src/stonefish_interface/src/turtlebot_teleop_convert_node.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import TwistStamped
+from std_msgs.msg import MultiArrayDimension, Float64MultiArray
+
+class VelocityConverter:
+    def __init__(self):
+        rospy.init_node('velocity_converter')
+        self.cmd_vel_sub = rospy.Subscriber('/cmd_vel', TwistStamped, self.cmd_vel_callback)
+        self.wheel_vel_pub = rospy.Publisher('/turtlebot/kobuki/commands/wheel_velocities', Float64MultiArray, queue_size=10)
+        self.d = 0.235   # Distance between wheels (example value, adjust according to your robot)
+        self.r = 0.035   # Radius of wheels (example value, adjust according to your robot)
+
+    def cmd_vel_callback(self, msg):
+        v = msg.twist.linear.x
+        w = msg.twist.angular.z
+        v_left = (v + (w * self.d) / 2) / self.r
+        v_right = (v - (w * self.d) / 2) / self.r
+
+        wheel_vel_msg = Float64MultiArray()
+        wheel_vel_msg.layout.dim.append(MultiArrayDimension())
+        wheel_vel_msg.layout.dim[0].size = 2
+        wheel_vel_msg.layout.dim[0].stride = 1
+        wheel_vel_msg.layout.dim[0].label = "wheel_velocities"
+        wheel_vel_msg.data = [v_left, v_right]
+
+        self.wheel_vel_pub.publish(wheel_vel_msg)
+
+if __name__ == '__main__':
+    try:
+        converter = VelocityConverter()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
