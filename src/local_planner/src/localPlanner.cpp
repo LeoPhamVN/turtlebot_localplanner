4957d66da6732fb270e3c21993018194cf414206
LeoPhamVN <phamthanhloc.bkhn@gmail.com>
2024-06-05 21:04:50 +0200

    deleted unnecesary code

--- src/local_planner/src/localPlanner.cpp
+++ src/local_planner/src/localPlanner.cpp
@@ -199,19 +199,6 @@ void odometryHandler(const nav_msgs::Odometry::ConstPtr& odom)
 void laserCloudHandler(const sensor_msgs::PointCloud2ConstPtr& laserCloud2)
 {
   if (!kUseTerrainAnalysis) {
-    // pcl::PointCloud<pcl::PointXYZ>::Ptr laserCloud(new pcl::PointCloud<pcl::PointXYZ>());
-    // pcl::fromROSMsg(*laserCloud2, *laserCloud);
-
-    // pointcloud_downsizer_.Downsize(laserCloud, 0.2, 0.2, 0.2);
-    // registered_cloud_->cloud_->clear();
-    // pcl::copyPointCloud(*laserCloud, *(registered_cloud_->cloud_));
-    // vertical_surface_cloud_->cloud_->clear();
-
-    // vertical_surface_extractor.ExtractVerticalSurface<PlannerCloudPointType, PlannerCloudPointType>(
-    //       registered_cloud_->cloud_, vertical_surface_cloud_->cloud_);
-
-    // pcl::PointCloud<pcl::PointXYZ>::Ptr vertical_surface_cloud(new pcl::PointCloud<pcl::PointXYZ>());
-    // pcl::copyPointCloud(*(vertical_surface_cloud_->cloud_), *vertical_surface_cloud);
 
     laserCloud->clear();
     pcl::fromROSMsg(*laserCloud2, *laserCloud);
@@ -263,20 +250,6 @@ void laserCloudHandler(const sensor_msgs::PointCloud2ConstPtr& laserCloud2)
 void laser2DCloudHandler(const sensor_msgs::LaserScanConstPtr& input)
 {
   if (!kUseTerrainAnalysis) {
-    // pcl::PointCloud<pcl::PointXYZ>::Ptr laserCloud(new pcl::PointCloud<pcl::PointXYZ>());
-    // pcl::fromROSMsg(*laserCloud2, *laserCloud);
-
-    // pointcloud_downsizer_.Downsize(laserCloud, 0.2, 0.2, 0.2);
-    // registered_cloud_->cloud_->clear();
-    // pcl::copyPointCloud(*laserCloud, *(registered_cloud_->cloud_));
-    // vertical_surface_cloud_->cloud_->clear();
-
-    // vertical_surface_extractor.ExtractVerticalSurface<PlannerCloudPointType, PlannerCloudPointType>(
-    //       registered_cloud_->cloud_, vertical_surface_cloud_->cloud_);
-
-    // pcl::PointCloud<pcl::PointXYZ>::Ptr vertical_surface_cloud(new pcl::PointCloud<pcl::PointXYZ>());
-    // pcl::copyPointCloud(*(vertical_surface_cloud_->cloud_), *vertical_surface_cloud);
-
     // Convert LaserScan to PointCloud2
     sensor_msgs::PointCloud2 cloud;
     projector.projectLaser(*input, cloud);
@@ -824,29 +797,9 @@ int main(int argc, char** argv)
     nhPrivate.getParam("kSensorDirZHIL", kSensorDirZ);
   }
   
-  
-  // Test
-  // ROS message filters
-  // message_filters::Subscriber<nav_msgs::Odometry> subOdometrySync;
-  // message_filters::Subscriber<sensor_msgs::PointCloud2> subLaserCloud;
-  // typedef message_filters::sync_policies::ApproximateTime<nav_msgs::Odometry, sensor_msgs::PointCloud2> syncPolicy;
-  // typedef message_filters::Synchronizer<syncPolicy> Sync;
-  // boost::shared_ptr<Sync> sync_;
-
-  // // SUBCRIBE
-  // subOdometrySync.subscribe(nh, sub_odometry_topic_, 1);
-
-  // subLaserCloud.subscribe(nh, "/sensor_registered_scan", 1);
-
-  // sync_.reset(new Sync(syncPolicy(100), subOdometrySync, subLaserCloud));
-  // sync_->registerCallback(boost::bind(laserCloudAndOdometryHandler, _1, _2));
-  // 
 
   ros::Subscriber subOdometry = nh.subscribe<nav_msgs::Odometry>
                                 (sub_odometry_topic_, 5, odometryHandler);
-
-  // ros::Subscriber subLaserCloud = nh.subscribe<sensor_msgs::PointCloud2>
-  //                                 (sub_registered_scan_topic_, 5, laserCloudHandler);
 
   ros::Subscriber subLaserCloud = nh.subscribe<sensor_msgs::LaserScan>
                                   (sub_registered_scan_topic_, 5, laser2DCloudHandler);
@@ -875,8 +828,6 @@ int main(int argc, char** argv)
   ros::Publisher pubGoalPointCloud =  nh.advertise<sensor_msgs::PointCloud2>(pub_goal_topic_, 10);
 
   nav_msgs::Path path;
-
-  pubLaserCloud = nh.advertise<sensor_msgs::PointCloud2>("test_pc", 2);
 
   #if PLOTPATHSET == 1
   ros::Publisher pubFreePaths = nh.advertise<sensor_msgs::PointCloud2> (pub_free_path_topic_, 2);
